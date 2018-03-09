6c3abe55e2f0c0f994095e658c7a52f60b581da7
Anthony Powell <apowell@arbor.net>
2018-03-09 17:03:50 -0500

    Implement font defaults and font-awesome icons
    -Choose starting google fonts: Roboto, Poppins
    -Implement FontAwesomeIcon component
    -Initialize FontAwesomeIcon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import { faSearch } from '@fortawesome/fontawesome-free-solid';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Spotlight</h1>
-        </header>
-        <p className="App-intro">
-          I'm nowhere near ready yet. PUNCH SOME CODE IN HERE!
-        </p>
-      </div>
-    );
-  }
-}
+/**
+ * Build a library of fontawesome icons
+ *
+ * This keeps bundle size low as we only bundle icons
+ * defined in this library.
+ *
+ * Icons in this library can be used by any fontawesome
+ * component
+ */
+fontawesome.library.add(brands, faSearch);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title-lg"><FontAwesomeIcon icon="search" size="8px" /> Spotlight</h1>
+    </header>
+    <p className="App-intro">
+      I&apos;m nowhere near ready yet. PUNCH SOME CODE IN HERE!
+    </p>
+  </div>
+);
 
 export default App;
