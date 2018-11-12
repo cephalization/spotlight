267acde3cd2f1a9584078484578271d61d40e51d
Anthony Powell <adpowell@mtu.edu>
2018-11-12 17:58:19 -0500

    Make navbar sticky when scrolling out of view
    - Convert PageHeader to stateful component
    -  Update package-lock from new npm version

--- src/Components/PageHeader/PageHeader.jsx
+++ src/Components/PageHeader/PageHeader.jsx
@@ -18,41 +18,80 @@ const styles = {
   },
 };
 
+const getElementWindowTop = elem => (
+  elem && typeof elem.getBoundingClientRect === 'function' ? elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top : 0
+);
+
 /**
  * Main Page Header and Brand
  */
-const PageHeader = ({ user, logout }) => (
-  <Segment
-    vertical
-    textAlign="center"
-    style={styles.headerSegment}
-  >
-    <Container text>
-      <Header
-        inverted
-        as="h1"
-        style={styles.headerBrand}
-      >
-        <Link className="hero" to="/"><FontAwesomeIcon icon="search" /> Spotlight</Link>
-      </Header>
-    </Container>
-    <Container className="top-pad">
-      <Menu inverted borderless fluid>
-        {user !== null
-            ? <Menu.Item><Image size="mini" circular src={_.get(user, ['images', 0, 'url'], '')} /></Menu.Item>
-            : null
-        }
-        {user !== null
-          ? <Menu.Item>{user.id}</Menu.Item>
-          : <Menu.Item as="a" href={loginEndpoint}>Login</Menu.Item>
-        }
-        {user !== null
-            ? <Menu.Item onClick={logout}>Logout</Menu.Item>
-            : null
-        }
-      </Menu>
-    </Container>
-  </Segment>
-);
+class PageHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sticky: false,
+    };
+
+    this.hero = React.createRef();
+    this.shouldBecomeSticky = this.shouldBecomeSticky.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.shouldBecomeSticky);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.shouldBecomeSticky);
+  }
+
+  /**
+   * When the hero container scrolls out of view, make navbar sticky
+   */
+  shouldBecomeSticky() {
+    const hero = this.hero.current;
+    const elementTop = getElementWindowTop(hero);
+    const elementBottom = elementTop + hero.clientHeight;
+
+    if (
+      elementBottom < window.scrollY
+      || elementTop >= (window.scrollY + window.innerHeight)
+    ) {
+      this.setState({ sticky: true });
+    } else if (this.state.sticky) {
+      this.setState({ sticky: false });
+    }
+  }
+
+  render() {
+    const { sticky } = this.state;
+
+    return (
+      <Segment vertical textAlign="center" style={styles.headerSegment}>
+        <div ref={this.hero}>
+          <Container text>
+            <Header inverted as="h1" style={styles.headerBrand}>
+              <Link className="hero" to="/"><FontAwesomeIcon icon="search" /> Spotlight</Link>
+            </Header>
+          </Container>
+        </div>
+        <Container className="top-pad">
+          <Menu inverted borderless fluid fixed={sticky ? 'top' : null}>
+            {this.props.user !== null ?
+              <Menu.Item><Image size="mini" circular src={_.get(this.props.user, ['images', 0, 'url'], '')} /></Menu.Item> : null
+            }
+            {this.props.user !== null ?
+              <Menu.Item>{this.props.user.id}</Menu.Item> : <Menu.Item as="a" href={loginEndpoint}>Login</Menu.Item>
+            }
+            {this.props.user !== null ?
+              <Menu.Item onClick={this.props.logout}>Logout</Menu.Item> : null
+            }
+          </Menu>
+        </Container>
+      </Segment>
+    );
+  }
+
+}
 
 export default withAuth(PageHeader);
