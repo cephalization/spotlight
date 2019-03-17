7a79096a9812a755796f2b679f4848d59e525434
Anthony <adpowell@mtu.edu>
2019-03-16 20:46:47 -0400

    Clean up code for stats page

--- src/Pages/StatsPage/StatsPage.jsx
+++ src/Pages/StatsPage/StatsPage.jsx
@@ -1,66 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { CardGroup, Tab } from 'semantic-ui-react';
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
 
 import { withAuth } from '../../Contexts/Authentication';
 import PageHeader from '../../Components/PageHeader/PageHeader';
 import PageFooter from '../../Components/PageFooter/PageFooter';
 import ContentSegment from '../../Components/ContentSegment/ContentSegment';
-import { topUserArtistsEndpoint } from '../../endpoints';
-import { saveGeneralAuth } from '../../utils';
-import ArtistCard from '../../Components/ArtistCard/ArtistCard';
+import TopArtistPane from './TopArtistPane';
 
-const StatsPage = ({ user, login, accessToken }) => {
+const TopTracksTab = <Tab.Pane>Coming soon...</Tab.Pane>;
+
+const panes = [
+  { menuItem: 'Top Artists (recently)', render: () => <TopArtistPane /> },
+  { menuItem: 'Top Tracks (recently)', render: () => TopTracksTab },
+];
+
+const StatsPage = ({ user, login }) => {
   if (!user) {
     login();
   }
 
-  const [loading, setLoading] = useState(true);
-  const [artists, setArtists] = useState([]);
-
-  useEffect(() => {
-    if (user) {
-      axios
-        .get(topUserArtistsEndpoint, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .then((response) => {
-          saveGeneralAuth(response.data.data.generalAuth);
-          setArtists(response.data.data.items);
-          setLoading(false);
-        })
-        .catch((error) => {
-          setLoading(false);
-          console.error('Failed to retrieve artist', error);
-        });
-    }
-  }, [JSON.stringify(user)]);
-
-  const TopArtistsTab = (
-    <Tab.Pane>
-      <CardGroup stackable centered itemsPerRow={4}>
-        {artists.map(artist => (
-          <ArtistCard artist={artist} key={artist.name} compact />
-        ))}
-      </CardGroup>
-    </Tab.Pane>
-  );
-  const TopTracksTab = <Tab.Pane>Coming soon...</Tab.Pane>;
-
-  const panes = [
-    { menuItem: 'Top Artists (recently)', render: () => TopArtistsTab },
-    { menuItem: 'Top Tracks (recently)', render: () => TopTracksTab },
-  ];
-
   return (
     <React.Fragment>
       <PageHeader />
-      <ContentSegment>
-        {loading && 'Loading...'}
-        {!loading && <Tab panes={panes} />}
-      </ContentSegment>
+      <ContentSegment>{<Tab panes={panes} />}</ContentSegment>
       <PageFooter />
     </React.Fragment>
   );
---
+++ src/Pages/StatsPage/TopArtistPane.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { CardGroup, Tab } from 'semantic-ui-react';
+
+import { topUserArtistsEndpoint } from '../../endpoints';
+import { saveGeneralAuth } from '../../utils';
+import ArtistCard from '../../Components/ArtistCard/ArtistCard';
+import { withAuth } from '../../Contexts/Authentication';
+
+const TopArtistPane = ({ user, accessToken }) => {
+  const [loading, setLoading] = useState(true);
+  const [artists, setArtists] = useState([]);
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(topUserArtistsEndpoint, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          saveGeneralAuth(response.data.data.generalAuth);
+          setArtists(response.data.data.items);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+          console.error('Failed to retrieve top artists', error);
+        });
+    }
+  }, [JSON.stringify(user)]);
+
+  return (
+    <Tab.Pane loading={loading}>
+      {!loading && (
+        <CardGroup stackable centered itemsPerRow={4}>
+          {artists.map(artist => (
+            <ArtistCard artist={artist} key={artist.name} compact />
+          ))}
+        </CardGroup>
+      )}
+    </Tab.Pane>
+  );
+};
+
+export default withAuth(TopArtistPane);
