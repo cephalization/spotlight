dfff57a7307044cf9cbebeae4ed9e7b3a2b73cdd
Anthony Powell <adpowell@mtu.edu>
2019-03-22 17:04:00 -0400

    Use relative link for callback

--- api/spotifyRequester.js
+++ api/spotifyRequester.js
@@ -3,10 +3,10 @@ const moment = require('moment');
 const request = require('request');
 const querystring = require('querystring');
 
-const NOW_URL =
-  process.env.NOW_URL != null && process.env.NOW_URL.length
-    ? process.env.NOW_URL
-    : 'http://localhost:3000';
+const BASE_URI =
+  process.env.DEBUG_URI != null && process.env.DEBUG_URI.length
+    ? process.env.DEBUG_URI
+    : '/';
 
 // Generate authorization request information for a GENERAL token
 const {
@@ -163,7 +163,7 @@ function spotifyLoginCallback() {
     const storedState = req.cookies ? req.cookies[spotifyStateKey] : null;
 
     if (state === null || state !== storedState) {
-      res.redirect(`${NOW_URL}/error_state_mismatch`);
+      res.redirect(`${BASE_URI}/error_state_mismatch`);
     } else {
       res.clearCookie(spotifyStateKey);
 
@@ -186,12 +186,12 @@ function spotifyLoginCallback() {
           const access_token = body.access_token;
           const refresh_token = body.refresh_token;
 
-          res.redirect(`${NOW_URL}/success?${querystring.stringify({
+          res.redirect(`${BASE_URI}/success?${querystring.stringify({
             access_token,
             refresh_token,
           })}`);
         } else {
-          res.redirect(`${NOW_URL}/error`);
+          res.redirect(`${BASE_URI}/error`);
         }
       });
     }
